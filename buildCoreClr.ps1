2ffc3f99fb05ee9c546b146559ce776fccdb47ab
Chris <c.bergmeister@gmail.com>
2017-12-02 21:22:32 +0000

    use dotnet publish instead of copying dlls

--- buildCoreClr.ps1
+++ buildCoreClr.ps1
@@ -31,21 +31,14 @@ function Invoke-RestoreSolution
     dotnet restore (Join-Path $PSScriptRoot .\PSScriptAnalyzer.sln)
 }
 
-$solutionDir = Split-Path $MyInvocation.InvocationName
-if (-not (Test-Path "$solutionDir/global.json"))
-{
-    throw "Not in solution root"
-}
+Push-Location $PSScriptRoot
 
-$itemsToCopyBinaries = @("$solutionDir\Engine\bin\$Configuration\$Framework\Microsoft.Windows.PowerShell.ScriptAnalyzer.dll",
-    "$solutionDir\Rules\bin\$Configuration\$Framework\Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules.dll")
+$itemsToCopyCommon = @("$PSScriptRoot\Engine\PSScriptAnalyzer.psd1",
+                       "$PSScriptRoot\Engine\PSScriptAnalyzer.psm1",
+                       "$PSScriptRoot\Engine\ScriptAnalyzer.format.ps1xml",
+                       "$PSScriptRoot\Engine\ScriptAnalyzer.types.ps1xml")
 
-$itemsToCopyCommon = @("$solutionDir\Engine\PSScriptAnalyzer.psd1",
-    "$solutionDir\Engine\PSScriptAnalyzer.psm1",
-    "$solutionDir\Engine\ScriptAnalyzer.format.ps1xml",
-    "$solutionDir\Engine\ScriptAnalyzer.types.ps1xml")
-
-$destinationDir = "$solutionDir\out\PSScriptAnalyzer"
+$destinationDir = "$PSScriptRoot\out\PSScriptAnalyzer"
 $destinationDirBinaries = $destinationDir
 if ($Framework -eq "netstandard1.6")
 {
@@ -63,7 +56,7 @@ if ($Restore.IsPresent)
 if ($build)
 {
 
-    if (-not (Test-DotNetRestore((Join-Path $solutionDir Engine))))
+    if (-not (Test-DotNetRestore((Join-Path $PSScriptRoot Engine))))
     {
         Invoke-RestoreSolution
     }
@@ -72,12 +65,13 @@ if ($build)
     Pop-Location
 
 
-    if (-not (Test-DotNetRestore((Join-Path $solutionDir Rules))))
+    if (-not (Test-DotNetRestore((Join-Path $PSScriptRoot Rules))))
     {
         Invoke-RestoreSolution
     }
     Push-Location Rules\
     dotnet build Rules.csproj --framework $Framework --configuration $Configuration
+    dotnet publish Rules.csproj --framework $Framework --configuration $Configuration --output $destinationDirBinaries
     Pop-Location
 
     Function CopyToDestinationDir($itemsToCopy, $destination)
@@ -92,16 +86,6 @@ if ($build)
         }
     }
     CopyToDestinationDir $itemsToCopyCommon $destinationDir
-    CopyToDestinationDir $itemsToCopyBinaries $destinationDirBinaries
-
-    # Copy Settings File
-    Copy-Item -Path "$solutionDir\Engine\Settings" -Destination $destinationDir -Force -Recurse -Verbose
-
-    # copy newtonsoft dll if net451 framework
-    if ($Framework -eq "net451")
-    {
-        copy-item -path "$solutionDir\Rules\bin\$Configuration\$Framework\Newtonsoft.Json.dll" -Destination $destinationDirBinaries -Verbose
-    }
 }
 
 $modulePath = "$HOME\Documents\WindowsPowerShell\Modules";
