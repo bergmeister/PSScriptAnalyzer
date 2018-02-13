2ea454639f846ab3d2296c6df6346caa4a4b065f
Christoph Bergmeister <c.bergmeister@gmail.com>
2018-02-13 22:35:43 +0000

    simplify and reduce nesting

--- Rules/PossibleIncorrectUsageOfAssignmentOperator.cs
+++ Rules/PossibleIncorrectUsageOfAssignmentOperator.cs
@@ -43,43 +43,52 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
             {
                 foreach (var clause in ifStatementAst.Clauses)
                 {
-                    var assignmentStatementAst = clause.Item1.Find(testAst => testAst is AssignmentStatementAst, searchNestedScriptBlocks: false) as AssignmentStatementAst;
-                    if (assignmentStatementAst != null)
+                    var ifStatementCondition = clause.Item1;
+                    var assignmentStatementAst = ifStatementCondition.Find(testAst => testAst is AssignmentStatementAst, searchNestedScriptBlocks: false) as AssignmentStatementAst;
+                    if (assignmentStatementAst == null)
                     {
-                        // Check if someone used '==', which can easily happen when the person is used to coding a lot in C#.
-                        // In most cases, this will be a runtime error because PowerShell will look for a cmdlet name starting with '=', which is technically possible to define
-                        if (assignmentStatementAst.Right.Extent.Text.StartsWith("="))
+                        continue;
+                    }
+                    // Check if someone used '==', which can easily happen when the person is used to coding a lot in C#.
+                    // In most cases, this will be a runtime error because PowerShell will look for a cmdlet name starting with '=', which is technically possible to define
+                    if (assignmentStatementAst.Right.Extent.Text.StartsWith("="))
+                    {
+                        yield return new DiagnosticRecord(
+                            Strings.PossibleIncorrectUsageOfComparisonOperatorAssignmentOperatorError, assignmentStatementAst.ErrorPosition,
+                            GetName(), DiagnosticSeverity.Warning, fileName);
+                    }
+                    else
+                    {
+                        var leftVariableExpressionAstOfAssignment = assignmentStatementAst.Left as VariableExpressionAst;
+                        if (leftVariableExpressionAstOfAssignment == null)
+                        {
+                            continue;
+                        }
+                        var statementBlockOfIfStatenent = clause.Item2;
+                        var variableExpressionAstsInStatementBlockOfIfStatement = statementBlockOfIfStatenent.FindAll(testAst => testAst is VariableExpressionAst, searchNestedScriptBlocks: true);
+                        if (variableExpressionAstsInStatementBlockOfIfStatement == null) // no variable uages
                         {
                             yield return new DiagnosticRecord(
                                 Strings.PossibleIncorrectUsageOfComparisonOperatorAssignmentOperatorError, assignmentStatementAst.ErrorPosition,
-                                GetName(), DiagnosticSeverity.Warning, fileName);
+                                GetName(), DiagnosticSeverity.Information, fileName);
                         }
                         else
                         {
-                            if (assignmentStatementAst.Left is VariableExpressionAst)
+                            var variableOnLHSIsBeingUsed = variableExpressionAstsInStatementBlockOfIfStatement.Where(x => x is VariableExpressionAst)
+                                                        .Any(x => ((VariableExpressionAst)x).VariablePath.UserPath.Equals(leftVariableExpressionAstOfAssignment.VariablePath.UserPath, StringComparison.OrdinalIgnoreCase));
+
+                            if (variableOnLHSIsBeingUsed)
                             {
-                                var vast = assignmentStatementAst.Left as VariableExpressionAst;
-                                var veAsts = clause.Item2.FindAll(testAst => testAst is VariableExpressionAst, searchNestedScriptBlocks: true);
-                                if (veAsts == null)
-                                {
-                                    yield return new DiagnosticRecord(
-                                       Strings.PossibleIncorrectUsageOfComparisonOperatorAssignmentOperatorError, assignmentStatementAst.ErrorPosition,
-                                       GetName(), DiagnosticSeverity.Information, fileName);
-                                }
-                                else
-                                {
-                                    // Check if LHS is being used
-                                    var variableIsBeingUsed = veAsts.Where(x => x is VariableExpressionAst).Any(x =>( (VariableExpressionAst)x).VariablePath.UserPath == vast.VariablePath.UserPath);
-                                    // If the right hand side contains a CommandAst at some point, then we do not want to warn
-                                    // because this could be intentional in cases like 'if ($a = Get-ChildItem){ }'
-                                    var commandAst = assignmentStatementAst.Right.Find(testAst => testAst is CommandAst, searchNestedScriptBlocks: true) as CommandExpressionAst;
-                                    if (commandAst == null && !variableIsBeingUsed)
-                                    {
-                                        yield return new DiagnosticRecord(
-                                            Strings.PossibleIncorrectUsageOfComparisonOperatorAssignmentOperatorError, assignmentStatementAst.ErrorPosition,
-                                            GetName(), DiagnosticSeverity.Information, fileName);
-                                    }
-                                }
+                                continue;
+                            }
+                            // If the right hand side contains a CommandAst at some point, then we do not want to warn
+                            // because this could be intentional in cases like 'if ($a = Get-ChildItem){ }'
+                            var commandAst = assignmentStatementAst.Right.Find(testAst => testAst is CommandAst, searchNestedScriptBlocks: true) as CommandExpressionAst;
+                            if (commandAst == null && !variableOnLHSIsBeingUsed)
+                            {
+                                yield return new DiagnosticRecord(
+                                    Strings.PossibleIncorrectUsageOfComparisonOperatorAssignmentOperatorError, assignmentStatementAst.ErrorPosition,
+                                    GetName(), DiagnosticSeverity.Information, fileName);
                             }
                         }
                     }
